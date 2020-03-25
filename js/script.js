46a814cfa96ad1718a803b418ae498f63868781d
Jair Alves <ajair2550@gmail.com>
2020-03-25 17:24:56 -0300

    3° Aula do Workshop da Rocketseat Concluído

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function onOff(){
     document.querySelector('#modal').classList.toggle('hide');
+
+    document.querySelector('body').classList.toggle('removeScroll');
+
+    document.querySelector('#modal').classList.toggle('addScroll');
 }
